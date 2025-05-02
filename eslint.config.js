92b84317a7d38214cd9fcd7aed06309cd535fbbe
Katya <gloooomrine@gmail.com>
2025-05-02 21:30:12 +0300

    docs: change ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,8 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import unicorn from "eslint-plugin-unicorn";
 import prettierConfig from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
-
 
 export default defineConfig([
   {
@@ -13,49 +11,25 @@ export default defineConfig([
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node
-      }
-    }
+        ...globals.node,
+      },
+    },
   },
 
   js.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  prettierConfig,
 
   {
-    plugins: {
-      unicorn,
-      "@typescript-eslint": tseslint.plugin
-    },
     rules: {
-      ...prettierConfig.rules,
-      
-      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-      "@typescript-eslint/no-explicit-any": "error",
-      
-      "unicorn/no-array-callback-reference": "off",
-      "unicorn/no-array-for-each": "off",
-      "unicorn/no-array-reduce": "off",
-      "unicorn/no-null": "off",
-      "unicorn/number-literal-case": "off",
-      "unicorn/prefer-modern-dom-apis": "error",
-      "unicorn/prefer-node-protocol": "error",
-      "unicorn/prefer-optional-catch-binding": "error",
-      "unicorn/no-for-loop": "error",
-      "unicorn/numeric-separators-style": "off",
-      "unicorn/prevent-abbreviations": [
+      "no-console": ["error", { allow: ["error"] }],
+      "import/prefer-default-export": ["off"],
+      "max-lines-per-function": ["error", 40],
+      "@typescript-eslint/restrict-template-expressions": [
         "error",
-        {
-          "allowList": {
-            "acc": true,
-            "env": true,
-            "i": true,
-            "j": true,
-            "props": true,
-            "Props": true
-          }
-        }
-      ]
-    }
-  }
+        { allowNumber: true },
+      ],
+    },
+  },
 ]);
