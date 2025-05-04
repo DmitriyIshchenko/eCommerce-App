58994cb2e72f7f21ffaa3b2ef4d275137d9cf145
Katya <gloooomrine@gmail.com>
2025-05-04 13:11:45 +0300

    docs: install vitest and create vite.config

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    assetsInlineLimit: 100000000,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'index.[ext]',
+        entryFileNames: 'index.js',
+      }
+    }
+  },
+  base: "/DmitriyIshchenko/eCommerce-App/",
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['src/setupTest.ts']
+  }
+});
