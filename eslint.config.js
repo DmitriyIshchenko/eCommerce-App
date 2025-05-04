c8119a190ca9c9e911afc725650a193443919fb5
Dmitriy Ishchenko <dmitriy.ishchenko1@gmail.com>
2025-05-04 21:55:11 +0300

    chore: RSS-ECOMM-1_24 Set up React and dependencies (#33)

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import prettierConfig from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig } from "eslint/config";
 
 import path from "path";
@@ -12,23 +14,25 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig([
-  { ignores: ["**/*.config.js"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommendedTypeChecked,
+  { ignores: ["**/*.config.js", "**/*.config.ts", "dist", "**/*.gen.ts"] },
   {
+    files: ["**/*.{ts,tsx}"],
+    plugins: { js, "react-hooks": reactHooks, "react-refresh": reactRefresh },
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2023,
       parserOptions: {
         project: true,
         tsconfigRootDir: __dirname,
       },
     },
-  },
-  pluginReact.configs.flat.recommended,
-  prettierConfig,
-
-  {
     rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
       "no-console": ["error", { allow: ["error"] }],
       "import/prefer-default-export": ["off"],
       "max-lines-per-function": ["error", 40],
@@ -37,5 +41,15 @@ export default defineConfig([
         { allowNumber: true },
       ],
     },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  prettierConfig,
 ]);
