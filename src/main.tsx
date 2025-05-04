4a9aedf9ee73cf13cb3732e7f14e139cf8da6a32
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2025-05-04 16:13:48 +0300

    chore: set up the fluent ui provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 import "./styles/globals.css";
 
 import { routeTree } from "./routeTree.gen";
@@ -19,6 +20,8 @@ document.body.append(root);
 
 createRoot(root).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <FluentProvider theme={webLightTheme}>
+      <RouterProvider router={router} />
+    </FluentProvider>
   </StrictMode>
 );
