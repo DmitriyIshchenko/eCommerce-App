8881d5cbfba26b8aca69933ae16c64f02cde430d
playoffthecuff <jmobile@bk.ru>
2025-05-11 09:05:19 +0300

    add simple test

---
+++ src/__tests__/1-simple-test.test.tsx
@@ -0,0 +1,10 @@
+import { describe, expect, test } from "vitest";
+import { countSum } from "./example-functions"; 
+
+describe("simple test", () => {
+  const a = 1;
+  const c = 3;
+  test("must correctly add 1 and 2", () => {
+    expect(countSum(a,2)).toBe(c);
+  })
+})
