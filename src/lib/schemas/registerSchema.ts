8d0f9db2a40726203a7177a891b00934ab396b23
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2025-05-16 15:14:09 +0300

    feat: create a register schema

---
+++ src/lib/schemas/registerSchema.ts
@@ -0,0 +1,45 @@
+import { z } from 'zod';
+import { loginSchema } from './auth';
+
+const patterns: Record<string, [RegExp, string]> = {
+  NO_SPECIAL_CHARS: [/^[^!@#$%^&*]*$/, 'Must contain no special characters'],
+  NO_NUMBERS: [/^[^\d]*$/, 'Must contain no numbers'],
+  POSTAL_CODE_US: [/\d+/, 'Must follow US postal code pattern (e.g. 12345)'],
+};
+
+const MIN_AGE = 13;
+
+export const registerSchema = loginSchema.extend({
+  firstName: z
+    .string()
+    .min(1, 'Required field')
+    .regex(...patterns.NO_SPECIAL_CHARS)
+    .regex(...patterns.NO_NUMBERS),
+  lastName: z
+    .string()
+    .min(1, 'Required field')
+    .regex(...patterns.NO_SPECIAL_CHARS)
+    .regex(...patterns.NO_NUMBERS),
+  dateOfBirth: z.string().refine((dateStr) => {
+    const date = new Date(dateStr);
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const pastDate = new Date();
+    pastDate.setFullYear(today.getFullYear() - MIN_AGE);
+    pastDate.setHours(0, 0, 0, 1);
+
+    return Number(date) - Number(pastDate) < 0;
+  }, `You must be at least ${MIN_AGE} years old`),
+
+  street: z.string().min(1, 'Required field'),
+  city: z
+    .string()
+    .regex(...patterns.NO_SPECIAL_CHARS)
+    .regex(...patterns.NO_NUMBERS),
+  postalCode: z.string().regex(...patterns.POSTAL_CODE_US),
+  country: z.string(),
+});
+
+export type RegisterSchema = z.infer<typeof registerSchema>;
