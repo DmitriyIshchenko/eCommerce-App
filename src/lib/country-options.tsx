32530cddd1bd4e91295f8bf44777581f0dfdbc5f
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2025-05-19 19:58:03 +0300

    refactor: update country options

--- src/lib/country-options.tsx
+++ src/lib/country-options.tsx
@@ -9,220 +9,22 @@ export const countries: Record<
     code: string;
   }
 > = {
-  AD: {
-    country: 'Andorra',
-    zipRegex: '^AD(\\d{3})$',
-    zipFormat: 'AD###',
-    code: 'AD',
-  },
-  AT: {
-    country: 'Austria',
-    zipRegex: '^\\d{4}$',
-    zipFormat: '####',
-    code: 'AT',
-  },
-  BE: {
-    country: 'Belgium',
-    zipRegex: '^\\d{4}$',
-    zipFormat: '####',
-    code: 'BE',
-  },
-  BR: {
-    country: 'Brazil',
-    zipRegex: '^\\d{5}-\\d{3}$',
-    zipFormat: '#####-###',
-    code: 'BR',
-  },
-  BG: {
-    country: 'Bulgaria',
-    zipRegex: '^(\\d{4})$',
-    zipFormat: '####',
-    code: 'BG',
-  },
-  CA: {
-    country: 'Canada',
-    zipRegex: '^[ABCEGHJKLMNPRSTVXY]\\d[ABCEGHJ-NPRSTV-Z] ?\\d[ABCEGHJ-NPRSTV-Z]\\d$',
-    zipFormat: 'A#A #A#',
-    code: 'CA',
-  },
-  HR: {
-    country: 'Croatia',
-    zipRegex: 'HR\\-(\\d{5})',
-    zipFormat: 'HR-#####',
-    code: 'HR',
-  },
-  CU: {
-    country: 'Cuba',
-    zipRegex: 'CP\\s(\\d{5})',
-    zipFormat: 'CP #####',
-    code: 'CU',
-  },
-  CY: {
-    country: 'Cyprus',
-    zipRegex: '^(\\d{4})$',
-    zipFormat: '####',
-    code: 'CY',
-  },
-  DK: {
-    country: 'Denmark',
-    zipRegex: '^\\d{4}$',
-    zipFormat: '####',
-    code: 'DK',
-  },
-  FR: {
-    country: 'France',
-    zipRegex: '^\\d{5}$',
-    zipFormat: '#####',
-    code: 'FR',
-  },
-  DE: {
-    country: 'Germany',
-    zipRegex: '^\\d{5}$',
-    zipFormat: '#####',
-    code: 'DE',
-  },
-  GR: {
-    country: 'Greece',
-    zipRegex: '^\\d{5}$',
-    zipFormat: '#####',
-    code: 'GR',
-  },
-  IS: {
-    country: 'Iceland',
-    zipRegex: '^\\d{3}$',
-    zipFormat: '###',
-    code: 'IS',
-  },
-  IT: {
-    country: 'Italy',
-    zipRegex: '^\\d{5}$',
-    zipFormat: '#####',
-    code: 'IT',
-  },
-  JP: {
-    country: 'Japan',
-    zipRegex: '^(\\d{3})-(\\d{4})$',
-    zipFormat: '###-####',
-    code: 'JP',
-  },
-  LV: {
-    country: 'Latvia',
-    zipRegex: '^LV-(\\d{4})$',
-    zipFormat: 'LV-####',
-    code: 'LV',
-  },
-  LT: {
-    country: 'Lithuania',
-    zipRegex: 'LT-(\\d{5})',
-    zipFormat: 'LT-#####',
-    code: 'LT',
-  },
-  MX: {
-    country: 'Mexico',
-    zipRegex: '^\\d{5}$',
-    zipFormat: '#####',
-    code: 'MX',
-  },
-  MC: {
-    country: 'Monaco',
-    zipRegex: '^\\d{5}$',
-    zipFormat: '#####',
-    code: 'MC',
-  },
-  LI: {
-    country: 'Liechtenstein',
-    zipRegex: '^\\d{4}$',
-    zipFormat: '####',
-    code: 'LI',
-  },
-  LU: {
-    country: 'Luxembourg',
-    zipRegex: '^\\d{4}$',
-    zipFormat: '####',
-    code: 'LU',
-  },
-  NL: {
-    country: 'Netherlands',
-    zipRegex: '^\\d{4} ?[A-Z]{2}$',
-    zipFormat: '#### AA',
-    code: 'NL',
-  },
-  NO: {
-    country: 'Norway',
-    zipRegex: '^\\d{4}$',
-    zipFormat: '####',
-    code: 'NO',
-  },
-  PL: {
-    country: 'Poland',
-    zipRegex: '^\\d{2}-\\d{3}$',
-    zipFormat: '##-###',
-    code: 'PL',
-  },
-  PT: {
-    country: 'Portugal',
-    zipRegex: '^\\d{4}-\\d{3}$',
-    zipFormat: '####-###',
-    code: 'PT',
-  },
-  RO: {
-    country: 'Romania',
-    zipRegex: '^(\\d{6})$',
+  BY: {
+    country: 'Belarus',
+    zipRegex: '^\\d{6}$',
     zipFormat: '######',
-    code: 'RO',
+    code: 'BY',
   },
-  SM: {
-    country: 'San Marino',
-    zipRegex: '^(4789\\d)$',
-    zipFormat: '4789#',
-    code: 'SM',
-  },
-  RS: {
-    country: 'Serbia',
-    zipRegex: '^(\\d{6})$',
+  RU: {
+    country: 'Russia',
+    zipRegex: '^\\d{6}$',
     zipFormat: '######',
-    code: 'RS',
-  },
-  SK: {
-    country: 'Slovakia',
-    zipRegex: '^(\\d{5})$',
-    zipFormat: '#####',
-    code: 'SK',
-  },
-  SI: {
-    country: 'Slovenia',
-    zipRegex: '^SI-(\\d{4})$',
-    zipFormat: 'SI-####',
-    code: 'SI',
-  },
-  ES: {
-    country: 'Spain',
-    zipRegex: '^\\d{5}$',
-    zipFormat: '#####',
-    code: 'ES',
-  },
-  SE: {
-    country: 'Sweden',
-    zipRegex: '^SE-(\\d{3})\\s(\\d{2})$',
-    zipFormat: 'SE-### ##',
-    code: 'SE',
-  },
-  CH: {
-    country: 'Switzerland',
-    zipRegex: '^\\d{4}$',
-    zipFormat: '####',
-    code: 'CH',
-  },
-  TR: {
-    country: 'Turkey',
-    zipRegex: '^\\d{5}$',
-    zipFormat: '#####',
-    code: 'TR',
+    code: 'RU',
   },
   US: {
     country: 'USA',
-    zipRegex: '^\\d{5}-(\\d{4})$',
-    zipFormat: '#####-####',
+    zipRegex: '^\\d{5}(-\\d{4})?$', // Improved to support both 5 and 9-digit formats
+    zipFormat: '##### or #####-####',
     code: 'US',
   },
 };
@@ -237,6 +39,5 @@ export const countryOptions = Object.values(countries).map((option) => ({
       {`${getUnicodeFlagIcon(option.code)} ${option.country}`}
     </option>
   ),
-
   value: option.code,
 }));
