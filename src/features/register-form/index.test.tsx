82a36bc6934047603c030d24af0fae6c9118e710
Olya Dubodel <dubodel.olya@gmail.com>
2025-05-30 19:20:32 +0300

    test: update previous tests

--- src/features/register-form/index.test.tsx
+++ src/features/register-form/index.test.tsx
@@ -11,6 +11,7 @@ import userEvent from '@testing-library/user-event';
 
 import RegisterForm from './index';
 import { UserContextProvider } from '../../components/contexts/user/context-provider';
+import { LoadingContextProvider } from '../../components/contexts/loading/context-provider';
 
 describe('Ensure that input validation checks are performed in real-time when the user enters their information.', () => {
   let emailField: HTMLInputElement;
@@ -53,9 +54,11 @@ describe('Ensure that input validation checks are performed in real-time when th
       path: '/',
       component: () => (
         <UserContextProvider>
-          <div data-testid="is-rendered">
-            <RegisterForm />
-          </div>
+          <LoadingContextProvider>
+            <div data-testid="is-rendered">
+              <RegisterForm />
+            </div>
+          </LoadingContextProvider>
         </UserContextProvider>
       ),
     });
