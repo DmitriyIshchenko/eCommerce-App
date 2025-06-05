ebdb869a6a45373528dd5822eb16e8e0a9ba20ad
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2025-06-05 18:55:24 +0300

    feat: restrict access to pages

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,8 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 import RegisterPage from '../pages/register';
+import { isTokenValid } from '../lib/api/token-storage';
 
 export const Route = createFileRoute('/register')({
   component: RouteComponent,
+  beforeLoad: () => {
+    if (isTokenValid()) {
+      throw redirect({ to: '/' });
+    }
+  },
 });
 
 function RouteComponent() {
